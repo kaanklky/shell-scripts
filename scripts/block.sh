0be6f9e51b180ef755990c31b33e1721d00ad25a
Kaan Kölköy <kaan@kaankolkoy.net>
2014-11-27 13:14:04 +0200

    added usage info to block.sh script

--- scripts/block.sh
+++ scripts/block.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#######################################
+## Usage: ~# ./block.sh <ip_address> ##
+#######################################   
 
 iptables -I INPUT -s $1 -j DROP
 echo $1 "adresi engellendi."
